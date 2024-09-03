3a42925a182d14bed1af2e1cdef767b591c05703
Elsaed Mohamed Elbarshly <saedelbarshly@gmail.com>
2024-09-03 13:16:46 +0300

    prebar

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'totle' => Str::random(50),
+            'body' => Str::random(255),
+        ]);
+    }
+}
